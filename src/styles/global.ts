d7f132564785e68124fdada9efd9ef71eb1fccac
Pedro Santos <pedrocarlos.ti@gmail.com>
2021-06-26 19:53:37 -0300

    add: Dashboard/Summary/Transactions page :tada:

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --red: #E52E4D;
         --blue: #5429CC;
         --blue-light: #6933FF;
+        --green: #33CC95;
 
         --text-title: #363F5F;
         --text-body: #969CB3;
@@ -30,7 +31,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background: var(---background);
+        background: var(--background);
         -webkit-font-smoothing: antialiased;
     }
 
