8a6f4c56df6f79472555b123407802b481e6c9ea
Pedro Santos <pedrocarlos.ti@gmail.com>
2021-06-29 19:29:19 -0300

    add: custom modal styles :nail_care:

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
         --text-title: #363F5F;
         --text-body: #969CB3;
-        
+
         --background: #F0F2F5;
         --shape: #FFFFFF;
     }
@@ -52,4 +52,40 @@ export const GlobalStyle = createGlobalStyle`
         opacity: 0.6;
         cursor: not-allowed;
     }
+
+		.react-modal-overlay {
+			background: rgba(0, 0, 0, 0.5);
+
+			position: fixed;
+			top: 0;
+			bottom: 0;
+			right: 0;
+			left: 0;
+
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		.react-modal-content {
+			width: 100%;
+			max-width: 576px;
+			background: var(--background);
+			border-radius: 6px;
+			padding: 3rem;
+			position: relative;
+		}
+
+		.react-modal-close {
+			position: absolute;
+			right: 1.5rem;
+			top: 1.5rem;
+			border: 0;
+			background: transparent;
+			transition: filter 0.2s;
+
+			:hover {
+				filter: brightness(0.75);
+			}
+		}
 `;
